ffdecae60f4329e9ca8ceb132136780f5b8ad463
Anthony Njuguna <tonju2@yahoo.com>
2021-04-25 13:46:45 -0500

    3rd version create circles

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -63,4 +63,14 @@ d3.csv("assets/data/data.csv").then(function(census) {
     chartGroup.append("g")
       .call(leftAxis);
 
-      
+// Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(hairData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "15")
+    .attr("fill", "pink")
+    .attr("opacity", ".5");
